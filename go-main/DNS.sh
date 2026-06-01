b4779e04d457f61a3c20cf2e122e80265d5bec27
rezamarzban <73051714+rezamarzban@users.noreply.github.com>
2026-06-01 13:24:35 +0330

    Delete go-main directory

--- go-main/DNS.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/SSH
-
-echo "DNS=193.186.32.32" | sudo tee -a /etc/systemd/resolved.conf
-
-sudo systemctl restart systemd-resolved
-
-sudo ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
