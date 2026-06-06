4d4bd899038e28eb2feb5252eacc1b35896916e8
rezamarzban <73051714+rezamarzban@users.noreply.github.com>
2026-06-06 15:51:52 +0330

    Delete uploads/do-main/do-main/alpine directory

--- uploads/do-main/do-main/alpine/README.md
+++
@@ -1,5 +0,0 @@
-sudo docker pull alpine:3.23
-
-sudo docker pull docker.arvancloud.ir/library/alpine:3.23
-
-sudo docker pull mirror2.chabokan.net/alpine:3.23
--- uploads/do-main/do-main/alpine/repos.sh
+++
@@ -1,3 +0,0 @@
-ALPINE_VERSION=$(cat /etc/alpine-release | cut -d'.' -f1-2)
-echo "https://mirror.arvancloud.ir/alpine/v${ALPINE_VERSION}/main" > /etc/apk/repositories
-echo "https://mirror.arvancloud.ir/alpine/v${ALPINE_VERSION}/community" >> /etc/apk/repositories
